35bb64447d9ef7f96a40e4ab7f277fc8e19e69cf
Andre Williams <awjrs22@gmail.com>
2020-04-17 10:52:26 -0700

    updates info concerning environment

--- spacy_summarization.py
+++ spacy_summarization.py
@@ -1,6 +1,7 @@
 # NLP Pkgs
+
 import spacy 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm') # terminal install: $ python -m spacy download en
 # Pkgs for Normalizing Text
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
